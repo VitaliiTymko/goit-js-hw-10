e40fa16196c333363d45fa19bfa7130508ca52a1
VitaliiTymko <intehmet2015@gmail.com>
2022-11-14 22:13:10 +0200

    apdate info

---
+++ src/fetchCountries.js
@@ -0,0 +1,5 @@
+// export default {fetchCountries};
+
+// function fetchCountries(name) {
+
+// }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
 import './css/styles.css';
-
+// import fetchCountries from "./fetchCountries";
 const DEBOUNCE_DELAY = 300;
+const refs = {
+  innerText: document.querySelector('#search-box'),
+  listCountry: document.querySelector('.country-list'),
+  infoCoutry: document.querySelector('.country-info'),
+};
+
+refs.innerText.addEventListener(
+  'input',
+  debounce(searchCoutry, DEBOUNCE_DELAY)
+);
+
+function searchCoutry(event) {
+  event.preventDefault();
+  const inputValue = refs.innerText.value.trim();
+  console.log(inputValue);
+}
+
+function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(responce => responce.json())
+    .then(data => console.log('name', name));
+}
+fetchCountries('sw');
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     // Data handling
+//   })
+//   .catch(error => {
+//     // Error handling
+//   });
