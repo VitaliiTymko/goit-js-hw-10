a8881c292046bdd9ddd66ace99d259c8da0ceaac
VitaliiTymko <intehmet2015@gmail.com>
2022-11-16 16:29:48 +0200

    add info

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,22 @@
-// export default {fetchCountries};
+export {fetchCountries};
+import Notiflix from 'notiflix';
+
+
+function fetchCountries(name){
+   return fetch (`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    .then((response) => {
+        if (!response.ok){
+            throw Error (response.status);
+        }
+        return response.json();
+    })
+    .catch (error =>{(Notiflix.Notify.failure("Oops, there is no country with that name"))})
+};
 
 // function fetchCountries(name) {
-
-// }
+//   return fetch(
+//     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+//   )
+//     .then(responce => responce.json())
+//     .then(data => console.log('name', name));
+// };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
 import './css/styles.css';
 // import fetchCountries from "./fetchCountries";
 const DEBOUNCE_DELAY = 300;
@@ -18,16 +19,62 @@ function searchCoutry(event) {
   event.preventDefault();
   const inputValue = refs.innerText.value.trim();
   console.log(inputValue);
+  fetchCountries(inputValue).then(renderСountryList).catch(console.log);
 }
 
-function fetchCountries(name) {
-  return fetch(
-    `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`
-  )
-    .then(responce => responce.json())
-    .then(data => console.log('name', name));
+// function fetchCountries(name) {
+//   return fetch(
+//     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+//   )
+//     .then(responce => responce.json())
+//     .then(data => console.log('name', name));
+// };
+
+function clear (){
+  refs.listCountry.innerHTML = '';
+  refs.infoCoutry.innerHTML = '';
 }
-fetchCountries('sw');
+
+function renderСountryList(backendResponse) {
+  console.log(backendResponse);
+  clear();
+  if (backendResponse.length > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  } else if (backendResponse.length === 1) {
+    refs.infoCoutry.innerHTML = renderCountryInfo(backendResponse[0]);
+  } else {
+    const renderListCountry = backendResponse
+      .map(country => renderCountriesList(country))
+      .join('');
+      refs.listCountry.insertAdjacentHTML('beforeend', renderListCountry);
+  }
+};
+
+function renderCountriesList({ flags, name }) {
+  return `<li class="country-listInfo">
+          <img class="country-flag" src="${flags.svg}" width="50" height="50"/>
+          <h2 class="country-listName">${name.official}</h2>
+          </li>`;
+}
+
+function renderCountryInfo({ name, flags, capital, population, languages }) {
+  return `<li class="country-firstInfo">
+          <div class="country-infoList">
+
+  <img class="country-flagInfo" src="${flags.svg}" width="300" height="300"/>
+  <h2 class="country-listName">${name.official}</h2>
+
+  <p><b> capital:</b> ${capital}</p>
+  <p><b> population:</b> ${population}</p>
+  <p><b> languages:</b> ${Object.values(languages)}</p>
+  </div>
+  </li>`;
+}
+
+// fetchCountries('sw');
+
 // fetch("https://jsonplaceholder.typicode.com/users")
 //   .then(response => {
 //     if (!response.ok) {
@@ -41,3 +88,5 @@ fetchCountries('sw');
 //   .catch(error => {
 //     // Error handling
 //   });
+
+// responseAPI
